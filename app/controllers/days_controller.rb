e2113f6ff6dbf46d25d753b9297dda5463c489c3
Katherine Rush <108463876+krushy8@users.noreply.github.com>
2026-03-03 12:06:05 +0900

    added days and weeks controllers

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,2 +1,21 @@
 class DaysController < ApplicationController
+  def show
+    @show = Show.find(params[:id])
+  end
+
+  def create
+    @show = Show.new(day_params)
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def day_params
+    params.require(day).permit(:date)
+  end
 end
--- app/controllers/weeks_controller.rb
+++ app/controllers/weeks_controller.rb
@@ -1,2 +1,10 @@
 class WeeksController < ApplicationController
+  def show
+    @week = Week.find(params[:id])
+  end
+
+  def create
+    @week = Week.new
+    @week.save
+  end
 end
