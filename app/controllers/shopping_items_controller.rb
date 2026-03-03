34cf5f48f5ec3593050a33824485dbbdd504978d
Koji Kaga <koji.farre@me.com>
2026-03-03 14:54:27 +0900

    minor fixes

--- app/controllers/shopping_items_controller.rb
+++ app/controllers/shopping_items_controller.rb
@@ -5,13 +5,17 @@ class ShoppingItemsController < ApplicationController
   end
 
   def update
-    @week = Week.find(params[:week_id])
-    @shopping_items = @week.shopping_items.find(params[:id])
-    @shopping_item.update
-    if @shopping_item.save
-      redirect_to shopping_items
+    @shopping_item = ShoppingItem.find(params[:id])
+    if @shopping_item.update(set_params)
+      redirect_to week_shopping_items_path(@shopping_item.week)
     else
       render :index, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def set_params
+    params.require(:shopping_item).permit(:purchased)
   end
 end
