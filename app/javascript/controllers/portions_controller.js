14746b23b11b017c917852bea882d560c402180f
Katherine Rush <108463876+krushy8@users.noreply.github.com>
2026-03-05 12:16:51 +0900

    added turbo spinner

--- app/javascript/controllers/portions_controller.js
+++ app/javascript/controllers/portions_controller.js
@@ -1,7 +1,23 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="portions"
 export default class extends Controller {
+  static targets = ["count"]
+
   connect() {
+    if (!this.hasCountTarget) return;
+    this.count = parseInt(this.countTarget.innerText) || 0;
+  }
+
+  increment() {
+    this.update(1)
+  }
+
+  decrement() {
+    this.update(-1)
+  }
+
+  update(amount) {
+    this.count += amount
+    this.countTarget.innerText = this.count
   }
 }
