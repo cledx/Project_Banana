5866b9699f92ff9174743b04eae60331c6e60017
Katherine Rush <108463876+krushy8@users.noreply.github.com>
2026-03-09 18:01:43 +0900

    added image on recipe view

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -2,9 +2,9 @@ class DaysController < ApplicationController
   def show
     @week = Week.find(params[:week_id])
     if params[:id].to_i.to_s == params[:id]
-      @day = @week.days.find(params[:id])
+      @day = @week.days.find_by(id: params[:id])
     else
-      @day = @week.days.where("date::date = ?", Date.parse(params[:id])).first
+      @day = @week.days.where("date::date = ?", Date.parse(params[:id]))
     end
     @dishes = @day.dishes
     redirect_to root_path, alert: "You are not authorized to access this day." if @day.week.user != current_user
