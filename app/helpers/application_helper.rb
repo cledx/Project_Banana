4f6a1f37068b48eb3f8b4684e42542deed15e064
Koji Kaga <koji.farre@me.com>
2026-03-10 12:17:36 +0900

    changed day-templates design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def render_markdown(text)
     Kramdown::Document.new(text, input: 'GFM', syntax_highlighter: "rouge").to_html
   end
+
+  def on_weeks_new?
+    params[:controller] == "weeks" && params[:action] == "new"
+  end
 end
