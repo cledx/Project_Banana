c0b019736a21ba923ef122763a928122b975195e
Katherine Rush <108463876+krushy8@users.noreply.github.com>
2026-03-11 11:58:34 +0900

    added action

--- app/javascript/controllers/week_select_controller.js
+++ app/javascript/controllers/week_select_controller.js
@@ -2,6 +2,31 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="week-select"
 export default class extends Controller {
-  connect() {
-  }
+  generate() {
+    const formData = {
+            day_templates: {
+              monday: { breakfast: 2, lunch: 2, dinner: 2 },
+              tuesday: { breakfast: 2, lunch: 2, dinner: 2 },
+              wednesday: { breakfast: 2, lunch: 2, dinner: 2 },
+              thursday: { breakfast: 2, lunch: 2, dinner: 2 },
+              friday: { breakfast: 2, lunch: 2, dinner: 2 },
+              saturday: { breakfast: 2, lunch: 2, dinner: 2 },
+              sunday: { breakfast: 2, lunch: 2, dinner: 2 }
+            }
+          }
+          // params[:day_templates][:monday][:breakfast] = 2
+
+          const url = `/weeks`
+
+          fetch(url, {
+            method: "POST",
+            headers:{
+              'Content-Type': 'application/json',
+              'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').content
+            },
+            body: JSON.stringify(formData)
+            }).then(response => {
+              if (response.redirected) window.location.href = response.url
+            })
+        }
 }
